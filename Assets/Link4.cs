b2f26efc17e40a3dc68ddd09996c8cf105eec441
Vince2137 <ahomoroc14@gmail.com>
2022-12-20 21:23:18 +0800

    Files Change

---
+++ Assets/Link4.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     public void LinkInfo4()
+    {
+        Application.OpenURL("https://en.wikipedia.org/wiki/Aloe_vera");
+    }
+}
+
---
+++ Assets/Links.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Links : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LinkInfo()
+    {
+        Application.OpenURL("https://en.wikipedia.org/wiki/File:Peace_lily_--_Spathiphyllum.jpg");
+    }
+}
